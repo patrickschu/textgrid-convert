81745a133d9f623edcb9a6941887a5face0283f6
patrick <you@example.com>
2019-12-09 22:33:37 -0600

    update srtParser to ABC Parser

--- textgrid_convert/srtParser.py
+++ textgrid_convert/srtParser.py
@@ -2,9 +2,11 @@
 # {id : "chunk", "start", "end"
 # expected out: 26 	 Carrie: 	 [78.28] 	 (pause 5.83) 	 [84.10]
 import logging
+import copy
 log = logging.getLogger(__name__)
+from textgrid_convert.ParserABC import ParserABC
 
-class srtParser(object):
+class srtParser(ParserABC):
     """
     Read and parse an srt formatted file
     Inofficial specs here: http://forum.doom9.org/showthread.php?p=470941#post470941
@@ -12,55 +14,30 @@ class srtParser(object):
         file_name(optional)
         srt_text(str)
     """
-    def __init__(self, srt_text):
+    def __init__(self, transcription):
         """
         Initializer
         Args:
             str_text(str)
         """
-        self.raw_srt=srt_text
-        self.parsed_srt= {}# containing srt content
+        self.transcription = transcription
 
-    def _to_textgrid_time(self, timestamp):
+
+    def parse_timestamp(self, timestamp):
         """
-        Output needs to be in mili seconds, round to 2
+        Convert from srt style timestamp 00:59:58,89 to ms
         Args:
-        00:52:58,579            timestamp (str)
-        Returns 
+            timestamp(str)
+        Return:
+            int
         """
-        if not isinstance(timestamp, str):
-            timestamp = str(timestamp)
         time, ms = timestamp.split(",")
-        hours, mins, secs = [float(i) for i in time.split(":")]
-        fulltime = (hours * 3600000) + (mins * 60000) + (secs * 1000) + float(ms)
+        hrs, mins, secs = [int(i) for i in time.split(":")]
+        fulltime = (hrs * 3600000) + (mins * 60000) + (secs * 1000) + int(ms)
         return fulltime
 
-    def to_textgrid(self, speaker_name="Speaker1"):
-        """
-        Convert to Praat Textgrid format
-        "Specs" here: http://www.fon.hum.uva.nl/praat/manual/Intro_7__Annotation.html
-        Time needs to be secs.milisecs, round to 2
-        Returns:
-            TextGrid compatible string
 
-        """
-        if len(self.parsed_srt) < 1:
-            self.parse_srt()
-        textgrid = []
-        intro_template = "{}     {}      "
-        time_template = "[{}]    {}      [{}]"
-        for chunk_id, values in self.parsed_srt.items():
-            # NUMBER\s\t\sSPEAKER\s\t\s[start]\s\tTEXT\s\t\s[end]\n 
-            intro = intro_template.format(chunk_id, speaker_name)
-            start_time, end_time = self._to_textgrid_time(values["start"]), self._to_textgrid_time(values["end"]) 
-            time = time_template.format(start_time, values["text"], end_time)
-            textgrid.append(intro + time)
-        assert len(textgrid) == len(self.parsed_srt), "make sure all items from parsed dictionary end up in list"
-        # FIXME this line sep
-        return "\n".join(textgrid) 
-
-
-    def parse_srt(self, srt_text=None, time_stamp_sep=" --> "):
+    def parse_transcription(self, srt_text=None, speaker_name="Speaker 1", time_stamp_sep=" --> "):
         """
         Pull the stuff from srt into a dictionary of format {chunk_id: {"text": "", "start": int, "end": int}}
         Args:
@@ -71,11 +48,12 @@ class srtParser(object):
             srt_text = self.raw_srt
         for chunk_id, timestamps, text in self.srt_generator(srt_text.splitlines(), separator=""):
             start, end = timestamps.split(time_stamp_sep)
-            self.parsed_srt[chunk_id] = {
+            self.transcription_dict[chunk_id] = {
+                    "speaker_name": speaker_name,
                     "start": start,  
                     "end": end,  
                     "text": text}
-        return self.parsed_srt.copy()
+        return copy.deepcopy(self.transcription_dict)
 
     def srt_generator(self, filein, separator="\n"):
         """
