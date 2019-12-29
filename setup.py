0b100983a8c07ddde7db6d24697129ec6bb57b67
patrick <you@example.com>
2019-12-29 12:26:49 +0100

    bump version

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ URL ="https://github.com/patrickschu/textgrid-convert/"
 EMAIL ="patrickschultz@utexas.edu"
 AUTHOR = "Patrick Schultz, Lars Hinrichs"
 REQUIRES_PYTHON = '>=3.0.0'
-VERSION = '0.1.0'
+VERSION = '0.2.0'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
