e6aaaa9f74568bee1404ef38b3ad250249cd7756
patrick <you@example.com>
2020-01-02 11:56:31 +0100

    bump version for zenodo

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2019, Patrick Schultz'
 author = 'Patrick Schultz'
 
 # The full version, including alpha/beta/rc tags
-release = '0.3.0'
+release = '0.4.1'
 
 
 # -- General configuration ---------------------------------------------------
